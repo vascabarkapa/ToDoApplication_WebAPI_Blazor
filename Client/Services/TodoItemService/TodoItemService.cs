435b54532aa8cb820f060874027add47210224e6
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-09-28 22:16:18 +0200

    get put post delete in service

--- Client/Services/TodoItemService/TodoItemService.cs
+++ Client/Services/TodoItemService/TodoItemService.cs
@@ -13,6 +13,45 @@ namespace ToDoApplication_WebAPI_Blazor.Client.Services.TodoItemService
 
         public List<TodoItem> TodoItems { get; set; } = new List<TodoItem>();
 
+        public async Task CreateTodoItem(TodoItem todoItem)
+        {
+            var result = await _http.PostAsJsonAsync("api/todoItem", todoItem);
+            var response = await result.Content.ReadFromJsonAsync<List<TodoItem>>();
+            
+            if(response != null)
+            {
+                TodoItems = response;
+            }
+
+            throw new Exception("Unexpected error on adding new Task");
+        }
+
+        public async Task UpdateTodoItem(TodoItem todoItem)
+        {
+            var result = await _http.PutAsJsonAsync($"api/todoItem/{todoItem.Id}", todoItem);
+            var response = await result.Content.ReadFromJsonAsync<List<TodoItem>>();
+
+            if (response != null)
+            {
+                TodoItems = response;
+            }
+
+            throw new Exception("Unexpected error on adding new Task");
+        }
+
+        public async Task DeleteTodoItem(int id)
+        {
+            var result = await _http.DeleteAsync($"api/todoItem/{id}");
+            var response = await result.Content.ReadFromJsonAsync<List<TodoItem>>();
+
+            if (response != null)
+            {
+                TodoItems = response;
+            }
+
+            throw new Exception("Unexpected error on adding new Task");
+        }
+
         public async Task<TodoItem> GetSingleTodoItem(int id)
         {
             var response = await _http.GetFromJsonAsync<TodoItem>($"api/todoItem/{id}");
