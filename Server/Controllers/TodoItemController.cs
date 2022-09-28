2f3b21db89639c6a75478c29fefafe993470d03e
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-09-28 19:44:10 +0200

    get todos and single todo

--- Server/Controllers/TodoItemController.cs
+++ Server/Controllers/TodoItemController.cs
@@ -11,5 +11,23 @@ namespace ToDoApplication_WebAPI_Blazor.Server.Controllers
             new TodoItem { Id=1, Name="Thesis", Description="Final the final thesis", DateAndTime=DateTime.Now, Priority="Medium", isDone=false },
             new TodoItem { Id=2, Name="Final", Description="Final the FEE", DateAndTime=DateTime.Now, Priority="High", isDone=false },
         };
+
+        [HttpGet]
+        public async Task<ActionResult<List<TodoItem>>> GetTodoItems()
+        {
+            return Ok(todoItems);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<TodoItem>>> GetSingleTodoItem(int id)
+        {
+            var todoItem = todoItems.FirstOrDefault(x => x.Id == id);
+
+            if(todoItem == null)
+            {
+                return NotFound("Sorry, there is not To Do Item");
+            }
+            return Ok(todoItem);
+        }
     }
 }
