d2efa411ad4844200ad0cf4670a4993ede04e778
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-09-28 19:33:05 +0200

    todo shared model

---
+++ Shared/TodoItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApplication_WebAPI_Blazor.Shared
+{
+    internal class TodoItem
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DateAndTime { get; set; }
+
+        public string Priority { get; set; }
+
+        public bool isDone { get; set; }
+    }
+}
